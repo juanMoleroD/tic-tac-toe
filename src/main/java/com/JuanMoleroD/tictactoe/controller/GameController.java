f5868ca24a69de17607ef768bd81c8f38dc11289
juanMoleroD <juanldimeco@gmail.com>
2022-02-17 20:40:27 +0000

    added gameplay session with websocket

--- src/main/java/com/JuanMoleroD/tictactoe/controller/GameController.java
+++ src/main/java/com/JuanMoleroD/tictactoe/controller/GameController.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
 
     private final GameService gameService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @PostMapping("/start")
     public ResponseEntity<Game> start(@RequestBody Player player){
@@ -44,9 +46,11 @@ public class GameController {
     }
 
     @PostMapping("/gameplay")
-    public ResponseEntity<Game> gameplay(@RequestBody Gameplay request) {
+    public ResponseEntity<Game> gameplay(@RequestBody Gameplay request) throws InvalidGameException, NotFoundException {
         log.info("gameplay: {}", request);
-
+        Game game = gameService.gameplay(request);
+        simpMessagingTemplate.convertAndSend("/topic/game-progress/" + game.getGameID(), game);
+        return ResponseEntity.ok(game);
     }
 
 }
