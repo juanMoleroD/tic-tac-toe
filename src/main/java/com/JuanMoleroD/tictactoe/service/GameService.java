fcd4f29a9e4f97cf95fb9343729de6b5ab286dd4
juanMoleroD <juanldimeco@gmail.com>
2022-02-17 19:35:26 +0000

    Added method to connect to game with ID and random game.

--- src/main/java/com/JuanMoleroD/tictactoe/service/GameService.java
+++ src/main/java/com/JuanMoleroD/tictactoe/service/GameService.java
@@ -1,6 +1,9 @@
 package com.JuanMoleroD.tictactoe.service;
 
 
+import com.JuanMoleroD.tictactoe.exception.InvalidGameException;
+import com.JuanMoleroD.tictactoe.exception.InvalidParamException;
+import com.JuanMoleroD.tictactoe.exception.NotFoundException;
 import com.JuanMoleroD.tictactoe.model.Game;
 import com.JuanMoleroD.tictactoe.model.GameStatus;
 import com.JuanMoleroD.tictactoe.model.Player;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+import static com.JuanMoleroD.tictactoe.model.GameStatus.IN_PROGRESS;
 import static com.JuanMoleroD.tictactoe.model.GameStatus.NEW;
 
 @Service
@@ -26,6 +30,33 @@ public class GameService {
         return game;
 
     }
+
+    public Game connectToGame(Player player2, String gameId) throws InvalidGameException, InvalidParamException {
+        if (!GameRepository.getInstance().getGames().containsKey(gameId)){
+            throw new InvalidParamException("Game not found with that ID");
+        }
+        Game game = GameRepository.getInstance().getGames().get(gameId);
+
+        if (game.getPlayer2() != null) {
+            throw new InvalidGameException("Game already full");
+        }
+
+        game.setPlayer2(player2);
+        game.setStatus(IN_PROGRESS);
+        GameRepository.getInstance().setGame(game);
+        return game;
+    }
+
+    public Game connectToRandomGame(Player player2) throws NotFoundException{
+        Game game = GameRepository.getInstance().getGames().values().stream()
+                .filter(it -> it.getStatus().equals(NEW)).findFirst()
+                .orElseThrow(() -> new NotFoundException("Game Not Found"));
+        game.setPlayer2(player2);
+        game.setStatus(IN_PROGRESS);
+        GameRepository.getInstance().setGame(game);
+        return game;
+    }
+
 
 
 }
